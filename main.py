bcdd7ba9ef88f9bee807b6110ad0d6ea5bc584f6
mrfriday05 <domonkos.pentek@gmail.com>
2025-05-14 18:33:59 +0200

    Presets

--- main.py
+++ main.py
@@ -10,38 +10,47 @@ ax.set_ylim(-10e7, 10e7)
 
 #Contants
 G=6.67430e-11 # [m3/kgs2]
-datapoints=30
+datapoints=1000
 dt=10
-cycles=10000
+cycles=25000
 storefrequency=40
 showfrequency=100
 imp=False
 export=False
 
 planets = [
-    Satellite(name="Earth", mass=5.972e+24,
-              pos=np.array([0.0, 0.0]),
-              vel=np.array([0.0, 0.0]),
+   Satellite(name="Asteroid1", mass=4.0e24,
+              pos=np.array([0.0, 4.27e7]),
+              vel=np.array([-1.8e3, -2.0e3]),
               datapoints=datapoints),
 
-    Satellite(name="Geostac", mass=1000,
-              pos=np.array([0.0,4.27e7]),
-              vel=np.array([3.0e3, 0.0]),
+    Satellite(name="Asteroid2", mass=4.0e24,
+              pos=np.array([0.0, 0.0]),
+              vel=np.array([2.0e3, -2.0e3]),
+              datapoints=datapoints),
+    Satellite(name="Asteroid2", mass=4.0e24,
+              pos=np.array([4.27e7, 0.0]),
+              vel=np.array([2.0e3, 2.0e3]),
+              datapoints=datapoints),
+    Satellite(name="Asteroid2", mass=4.0e24,
+              pos=np.array([4.27e7, 4.27e7]),
+              vel=np.array([-2.0e3, 2.0e3]),
               datapoints=datapoints)
 ]
 
 if imp==True:
-    with open('Preset1.json', 'r') as fin:
+    with open('Threebody1.json', 'r') as fin:
         importlst=json.load(fin)
     planets=importjson(importlst,datapoints)
+imp=False
 
 
 
 
 if export==True:
     out_lst = json.dumps([p.__dict__() for p in planets], indent=4)
-    with open("Sat1.json", "w") as fout:
-        fin.write(out_lst)
+    with open("Fourbody3.json", "w") as fout:
+        fout.write(out_lst)
 
 
 lineheads = [ax.plot([], [], 'o', markersize=6)[0] for _ in planets]
